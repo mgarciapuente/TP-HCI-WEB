deff6f25ec215e4e9c9900cd56fa83240d52f9b8
theras <theras@itba.edu.ar>
2025-10-08 19:34:56 -0300

    modularize lists page and products page

---
+++ src/components/ListaProductos.vue
@@ -0,0 +1,37 @@
+
+<script>
+    export default {
+        props: ['selectedList']
+}
+</script>
+
+<template>
+    <div class="products-panel">
+        <div v-if="!selectedList" class="no-selection">
+            <v-icon size="64" color="grey">mdi-format-list-bulleted</v-icon>
+            <h3>Seleccione una lista para ver los productos</h3>
+        </div>
+        <div v-else class="products-content">
+            <h3 class="panel-title">{{ selectedList.name }}</h3>
+            <div class="products-scroll">
+                <div v-if="selectedList.products.length === 0" class="no-products">
+                    <v-icon size="48" color="grey">mdi-cart-outline</v-icon>
+                    <p class="text-grey">No hay productos en esta lista aún</p>
+                    <p class="text-grey-lighten-1">Usa el botón + de la derecha para agregar productos</p>
+                </div>
+                <div v-else class="products-list">
+                    <v-card v-for="product in selectedList.products" :key="product.id" class="product-item mb-2"
+                        variant="flat">
+                        <v-card-text class="product-content">
+                            <div class="product-info">
+                                <h4 class="product-name">{{ product.name }}</h4>
+                                <p class="product-details">{{ product.quantity }} {{ product.unit }}</p>
+                            </div>
+                            <v-checkbox density="compact" hide-details></v-checkbox>
+                        </v-card-text>
+                    </v-card>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
--- src/pages/ProductosPage.vue
+++ src/pages/ProductosPage.vue
@@ -1,14 +1,20 @@
 <template>
-  <div class="productos-page">
-    <h1>Productos</h1>
-    <p>Gestiona todos tus productos desde aquí.</p>
-    
-    <!-- Placeholder para el contenido de productos -->
-    <div class="content-placeholder">
-      <v-icon size="64" color="grey">mdi-package-variant</v-icon>
-      <h3>Gestión de productos en desarrollo</h3>
+  <h1>Productos</h1>
+  <div>
+    <div>
+      <v-text-field type="text" name="search-bar" id="" prepend-inner-icon="$plus" />
+      <v-btn>+ Agregar Manualmente</v-btn>
+    </div>
+    <div>
+      <div>
+
+      </div> 
+      <div>
+        
+      </div>
     </div>
   </div>
+
 </template>
 
 <script setup lang="ts">
@@ -16,16 +22,5 @@
 </script>
 
 <style scoped>
-.productos-page {
-  padding: 20px;
-}
 
-.content-placeholder {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 300px;
-  color: #666;
-}
 </style>
